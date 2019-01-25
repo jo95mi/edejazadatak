15d23f8406036c8b02380fd7e14692d80eedc1d4
Jovan <milojevic95jovan@gmail.com>
2019-01-25 18:55:00 +0100

    Project creation
    Created the project.

---
+++ EdejaZadatak/EdejaZadatak/Controllers/FakturasController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EdejaZadatak.Models
+{
+    public class FakturasController : Controller
+    {
+        private BazaEdejaZadatakEntities db = new BazaEdejaZadatakEntities();
+
+        // GET: Fakturas
+        public ActionResult Index()
+        {
+            return View(db.Fakturas.ToList());
+        }
+
+        // GET: Fakturas/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Faktura faktura = db.Fakturas.Find(id);
+            if (faktura == null)
+            {
+                return HttpNotFound();
+            }
+            return View(faktura);
+        }
+
+        // GET: Fakturas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Fakturas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BrojFakture,DatumFakture,Ukupno")] Faktura faktura)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Fakturas.Add(faktura);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(faktura);
+        }
+
+        // GET: Fakturas/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Faktura faktura = db.Fakturas.Find(id);
+            if (faktura == null)
+            {
+                return HttpNotFound();
+            }
+            return View(faktura);
+        }
+
+        // POST: Fakturas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BrojFakture,DatumFakture,Ukupno")] Faktura faktura)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(faktura).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(faktura);
+        }
+
+        // GET: Fakturas/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Faktura faktura = db.Fakturas.Find(id);
+            if (faktura == null)
+            {
+                return HttpNotFound();
+            }
+            return View(faktura);
+        }
+
+        // POST: Fakturas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Faktura faktura = db.Fakturas.Find(id);
+            db.Fakturas.Remove(faktura);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EdejaZadatak/EdejaZadatak/Controllers/StavkasController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EdejaZadatak.Models
+{
+    public class StavkasController : Controller
+    {
+        private BazaEdejaZadatakEntities db = new BazaEdejaZadatakEntities();
+
+        // GET: Stavkas
+        public ActionResult Index()
+        {
+            var stavkas = db.Stavkas.Include(s => s.Faktura);
+            return View(stavkas.ToList());
+        }
+
+        // GET: Stavkas/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Stavka stavka = db.Stavkas.Find(id);
+            if (stavka == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stavka);
+        }
+
+        // GET: Stavkas/Create
+        public ActionResult Create()
+        {
+            ViewBag.BrojFakture = new SelectList(db.Fakturas, "BrojFakture", "BrojFakture");
+            return View();
+        }
+
+        // POST: Stavkas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BrojFakture,RedniBroj,Kolicina,Cena,Ukupno")] Stavka stavka)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Stavkas.Add(stavka);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.BrojFakture = new SelectList(db.Fakturas, "BrojFakture", "BrojFakture", stavka.BrojFakture);
+            return View(stavka);
+        }
+
+        // GET: Stavkas/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Stavka stavka = db.Stavkas.Find(id);
+            if (stavka == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.BrojFakture = new SelectList(db.Fakturas, "BrojFakture", "BrojFakture", stavka.BrojFakture);
+            return View(stavka);
+        }
+
+        // POST: Stavkas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BrojFakture,RedniBroj,Kolicina,Cena,Ukupno")] Stavka stavka)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(stavka).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.BrojFakture = new SelectList(db.Fakturas, "BrojFakture", "BrojFakture", stavka.BrojFakture);
+            return View(stavka);
+        }
+
+        // GET: Stavkas/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Stavka stavka = db.Stavkas.Find(id);
+            if (stavka == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stavka);
+        }
+
+        // POST: Stavkas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Stavka stavka = db.Stavkas.Find(id);
+            db.Stavkas.Remove(stavka);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
