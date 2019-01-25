273471f4bbf9c7787c118ef4180136caaad03b10
Jovan <milojevic95jovan@gmail.com>
2019-01-25 19:31:54 +0100

    Config files VS

--- EdejaZadatak/EdejaZadatak/Controllers/FakturasController.cs
+++ EdejaZadatak/EdejaZadatak/Controllers/FakturasController.cs
@@ -71,7 +71,6 @@ namespace EdejaZadatak.Models
             }
             return View(faktura);
         }
-
         // POST: Fakturas/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
